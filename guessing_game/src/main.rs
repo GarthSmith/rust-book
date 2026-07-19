0a1762cd2cfd1b75c42948bda539a72ebfde5bb2
Garth Smith <garth@tictocgames.com>
2026-07-19 16:28:40 -0700

    Completed Chapter 2

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,16 +1,41 @@
-
+use std::cmp::Ordering;
 use std::io;
+
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
 
-    println!("Please input your guess.");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    let mut guess = String::new(); // String is a growable, UTF-8 encoded bit of text.
+    loop {
+        println!("Please input your guess.");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        let mut guess = String::new(); // String is a growable, UTF-8 encoded bit of text.
 
-    println!("You guessed: {guess}");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        if guess.trim() == "/exit" {
+            println!("Exiting game.");
+            break;
+        }
+
+        let guess: i64 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
